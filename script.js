bfa9b54c98bad0c355f7a1e013e3a55230aa9cbb
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-02 11:59:38 +0000

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+// Auto-typing effect
+const typed = new Typed('.auto-type', {
+    strings: ["Programmer", "Robotics Enthusiast", "Problem Solver", "Youtuber", "Photographer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
+
