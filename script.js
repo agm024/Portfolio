b238cc01b6466377ca68f9cc2a10f9e008e59688
Agastya Malunjkar <161752478+agm024@users.noreply.github.com>
2025-02-19 15:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const typed=new Typed(".auto-type",{strings:["Programmer","Robotics Enthusiast","Problem Solver","Youtuber","Photographer"],typeSpeed:100,backSpeed:50,loop:!0});
 
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-0N1K45P49S');
